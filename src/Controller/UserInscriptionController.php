ac08d44403e19a2257e57240926bf5eeb187abb5
zych.johann@gmail.com <zych.johann@gmail.com>
2022-10-27 14:47:17 +0200

    US Inscription User sans CSS

---
+++ src/Controller/UserInscriptionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+use PDO;
+
+class UserInscriptionController extends AbstractController
+{
+    public function add(): ?string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $user = array_map('trim', $_POST);
+
+            $userManager = new UserManager();
+            $userManager->insert($user);
+
+            header('Location:/public/index.php');
+            return null;
+        }
+        return $this->twig->render('userInscription.html.twig');
+    }
+}
---
+++ src/Model/UserManager.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class UserManager extends AbstractManager
+{
+    public const TABLE = 'user';
+
+    /**
+     * Insert new item in database
+     */
+    public function insert(array $user): void
+    {
+        $statementUser = $this->pdo->prepare("INSERT INTO User
+    (`email`, `password` , `avatar`,`activationCode`, `isAdmin`,`firstname` , `lastname` , `telephone` , `address` )
+VALUES
+    (:email, :password , :avatar ,:activationCode, :isAdmin , :firstname , :lastname , :telephone , :address)");
+
+        $statementUser->bindValue('email', $user['emailUserInscription'], PDO::PARAM_STR);
+        $statementUser->bindValue('password', $user['password'], PDO::PARAM_STR);
+        $statementUser->bindValue('avatar', 1, PDO::PARAM_INT);
+        $statementUser->bindValue('activationCode', $user['activationCode'], PDO::PARAM_INT);
+        $statementUser->bindValue('isAdmin', false, PDO::PARAM_BOOL);
+        $statementUser->bindValue('firstname', $user['firstname'], PDO::PARAM_STR);
+        $statementUser->bindValue('lastname', $user['lastname'], PDO::PARAM_STR);
+        $statementUser->bindValue('telephone', $user['telephone'], PDO::PARAM_INT);
+        $statementUser->bindValue('address', $user['address'], PDO::PARAM_STR);
+        $statementUser->execute();
+
+
+        $statementUserId = $this->pdo->query("SELECT userID FROM User
+              WHERE email = '" . $user['emailUserInscription'] . "'");
+        $id = $statementUserId->fetch();
+
+        $statementKid = $this->pdo->prepare("INSERT INTO Kid
+    (`userID`,`firstname`, `birthday` , `specs`)
+VALUES
+    (:userID , :firstname, :birthday , :specs)");
+        $statementKid->bindValue('userID', $id['userID'], PDO::PARAM_INT);
+        $statementKid->bindValue('firstname', $user['firstnameKid'], PDO::PARAM_STR);
+        $statementKid->bindValue('birthday', $user['birthdayKid'], PDO::PARAM_STR);
+        $statementKid->bindValue('specs', $user['commentKid'], PDO::PARAM_STR);
+        $statementKid->execute();
+    }
+}
