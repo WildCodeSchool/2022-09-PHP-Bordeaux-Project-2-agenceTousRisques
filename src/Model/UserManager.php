90d4983e31d37d9b900e3ad3cb92c0fde4274094
zych.johann@gmail.com <zych.johann@gmail.com>
2022-10-29 17:13:05 +0200

    Validation data

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -11,29 +11,29 @@ class UserManager extends AbstractManager
     /**
      * Insert new item in database
      */
-    public function insert(array $user): void
+    public function insertUser(array $user): void
     {
         $statementUser = $this->pdo->prepare("INSERT INTO User
     (`email`, `password` , `avatar`,`activationCode`, `isAdmin`,`firstname` , `lastname` , `telephone` , `address` )
 VALUES
     (:email, :password , :avatar ,:activationCode, :isAdmin , :firstname , :lastname , :telephone , :address)");
 
-        $statementUser->bindValue('email', $user['emailUserInscription'], PDO::PARAM_STR);
-        $statementUser->bindValue('password', $user['password'], PDO::PARAM_STR);
+        $statementUser->bindValue('email', htmlspecialchars($_GET['email']), FILTER_VALIDATE_EMAIL);
+        $statementUser->bindValue('password', htmlspecialchars($user['password']), PDO::PARAM_STR);
         $statementUser->bindValue('avatar', 1, PDO::PARAM_INT);
-        $statementUser->bindValue('activationCode', $user['activationCode'], PDO::PARAM_INT);
+        $statementUser->bindValue('activationCode', htmlspecialchars($_GET['code']), PDO::PARAM_INT);
         $statementUser->bindValue('isAdmin', false, PDO::PARAM_BOOL);
-        $statementUser->bindValue('firstname', $user['firstname'], PDO::PARAM_STR);
-        $statementUser->bindValue('lastname', $user['lastname'], PDO::PARAM_STR);
-        $statementUser->bindValue('telephone', $user['telephone'], PDO::PARAM_INT);
-        $statementUser->bindValue('address', $user['address'], PDO::PARAM_STR);
+        $statementUser->bindValue('firstname', htmlspecialchars($user['firstname']), PDO::PARAM_STR);
+        $statementUser->bindValue('lastname', htmlspecialchars($user['lastname']), PDO::PARAM_STR);
+        $statementUser->bindValue('telephone', htmlspecialchars($user['telephone']), PDO::PARAM_INT);
+        $statementUser->bindValue('address', htmlspecialchars($user['address']), PDO::PARAM_STR);
         $statementUser->execute();
     }
 
     public function getUserID($user)
     {
         $statementUserId = $this->pdo->query("SELECT userID FROM User
-              WHERE email = '" . $user['emailUserInscription'] . "'");
+              WHERE email = '" . htmlspecialchars($user['emailUserInscription']) . "'");
         return $statementUserId->fetch();
     }
 
@@ -43,10 +43,10 @@ VALUES
     (`userID`,`firstname`, `birthday` , `specs`)
 VALUES
     (:userID , :firstname, :birthday , :specs)");
-        $statementKid->bindValue('userID', $id['userID'], PDO::PARAM_INT);
-        $statementKid->bindValue('firstname', $user['firstnameKid'], PDO::PARAM_STR);
-        $statementKid->bindValue('birthday', $user['birthdayKid'], PDO::PARAM_STR);
-        $statementKid->bindValue('specs', $user['commentKid'], PDO::PARAM_STR);
+        $statementKid->bindValue('userID', htmlspecialchars($id['userID']), PDO::PARAM_INT);
+        $statementKid->bindValue('firstname', htmlspecialchars($user['firstnameKid']), PDO::PARAM_STR);
+        $statementKid->bindValue('birthday', htmlspecialchars($user['birthdayKid']), PDO::PARAM_STR);
+        $statementKid->bindValue('specs', htmlspecialchars($user['commentKid']), PDO::PARAM_STR);
         $statementKid->execute();
     }
 }
