168606f2fcab1ad37d0ec39843a5a3964c2cbe4e
Jordan Lassalle <lassalle.jordan@gmail.com>
2022-11-06 20:16:05 +0100

    fixed and improved the header with an animated burger menu

--- public/assets/js/headerMenu.js
+++ public/assets/js/headerMenu.js
@@ -1,11 +1,16 @@
 let $button = document.getElementById("menu-btn")
+let $menu = document.getElementById("menu-list")
 $button.addEventListener('click', function (e) {
     e.preventDefault();
     if ($button.classList.contains("open")) {
         $button.classList.add("close");
         $button.classList.remove("open");
+        $menu.classList.add("close");
+        $menu.classList.remove("open");
     } else {
         $button.classList.add("open");
         $button.classList.remove("close");
+        $menu.classList.add("open");
+        $menu.classList.remove("close");
     }
 });
