ec98bceb8741f30bc5e1acb3c961d90f41da19c5
yazidham <79407250+yazidham@users.noreply.github.com>
2022-11-09 16:48:02 +0100

    finished the userCalls feature and fixed the margin problem

--- src/Model/UserCallsModel.php
+++ src/Model/UserCallsModel.php
@@ -4,9 +4,13 @@ namespace App\Model;
 
 class UserCallsModel extends AbstractManager
 {
-    public function getCalls()
+    public function getCalls(): array
     {
-        $query = "SELECT * FROM `call` WHERE userID !=" . $_SESSION['user_id'] . " ORDER BY startdate";
+        $query = "SELECT U.firstname, startdate, enddate, context, comment, callID
+            FROM `call`
+            INNER JOIN `user` U on `call`.userID = U.userID
+            WHERE U.UserID != " . $_SESSION['user_id'] . " AND helperID IS NULL
+            ORDER BY startdate";
         $statement = $this->pdo->prepare($query);
         $statement->execute();
         $calls = $statement->fetchAll();
