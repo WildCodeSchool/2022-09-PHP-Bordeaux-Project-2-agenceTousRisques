907c0407ebba3ac4b7b5c22a1ec28ab7934547ff
zych.johann@gmail.com <zych.johann@gmail.com>
2022-11-10 16:10:37 +0100

    Fix database.sql

--- database.sql
+++ database.sql
@@ -77,16 +77,16 @@ ALTER TABLE `Call` ADD CONSTRAINT `fk_Call_helperID` FOREIGN KEY(`helperID`)
 
 
 INSERT INTO Avatar (avatarID, image)
-VALUES (1, 'assets/avatar1.png'),
-       (2, 'assets/avatar2.png'),
-       (3, 'assets/avatar3.png'),
-       (4, 'assets/avatar4.png'),
-       (5, 'assets/avatar5.png'),
-       (6, 'assets/avatar6.png'),
-       (7, 'assets/avatar7.png'),
-       (8, 'assets/avatar8.png'),
-       (9, 'assets/avatar9.png'),
-       (10, 'assets/avatar10.png');
+VALUES (1, 'assets/images/avatar1.png'),
+       (2, 'assets/images/avatar2.png'),
+       (3, 'assets/images/avatar3.png'),
+       (4, 'assets/images/avatar4.png'),
+       (5, 'assets/images/avatar5.png'),
+       (6, 'assets/images/avatar6.png'),
+       (7, 'assets/images/avatar7.png'),
+       (8, 'assets/images/avatar8.png'),
+       (9, 'assets/images/avatar9.png'),
+       (10, 'assets/images/avatar10.png');
 
 INSERT INTO User (firstname, lastname, telephone, address, email, password, avatar, activationcode, isAdmin, isVisible)
 VALUES ('Jean Le Grand', 'Bokassa', '0102030405', '171 Rue Lucien Faure 33000 Bordeaux',
@@ -121,29 +121,29 @@ VALUES ('Jean Le Grand', 'Bokassa', '0102030405', '171 Rue Lucien Faure 33000 Bo
         '0000009', 0, 1);
 
 INSERT INTO Kid (userID, firstname, birthday, specs)
-VALUES (1, 'Michel', '14/01/2020', 'Rien de particulier'),
-       (2, 'Jules', '31/12/2021', 'Asthmatique'),
-       (3, 'Noah', '28/11/2021', 'Rien'),
-       (4, 'Céline', '12/02/2020', 'Jumeaux'),
-       (4, 'Julien', '12/02/2020', 'Jumeaux'),
-       (5, 'Marie', '01/02/2021', 'Rien'),
-       (6, 'Mickaël', '05/04/2020', 'Diabète'),
-       (7, 'Pierre', '27/04/2021', 'Rien'),
-       (8, 'Antony', '15/08/2020', 'Anxieux'),
-       (9, 'Sophie', '17/07/2020', 'Nothing'),
-       (10, 'Guenièvre', '01/06/2020', 'Reine'),
-       (10, 'Arthur', '01/06/2020', 'Roi');
+VALUES (1, 'Michel', '2020-04-11', 'Rien de particulier'),
+       (2, 'Jules', '2021-12-31', 'Asthmatique'),
+       (3, 'Noah', '2021-11-28', 'Rien'),
+       (4, 'Céline', '2020-02-12', 'Jumeaux'),
+       (4, 'Julien', '2020-02-12', 'Jumeaux'),
+       (5, 'Marie', '2021-02-01', 'Rien'),
+       (6, 'Mickaël', '2020-05-04', 'Diabète'),
+       (7, 'Pierre', '2021-04-27', 'Rien'),
+       (8, 'Antony', '2021-08-15', 'Anxieux'),
+       (9, 'Sophie', '2020-07-17', 'Nothing'),
+       (10, 'Guenièvre', '2020-01-06', 'Reine'),
+       (10, 'Arthur', '2020-01-06', 'Roi');
 
 INSERT INTO `Call` (userID, helperID, startdate, enddate, context, comment)
-VALUES (1, null, '12/11/2022 09:00', '13/11/2022 09:00', 'Absence Parents', 'Rien de particulier'),
-       (8, 4, '12/11/2022 09:00', '13/11/2022 09:00', 'Absence Parents', 'Rien de particulier'),
-       (8, null, '17/11/2022 09:00', '18/11/2022 09:00', 'Absence Parents', 'Rien de particulier'),
-       (8, null, '21/11/2022 09:00', '22/11/2022 09:00', 'Absence Parents', 'Rien de particulier'),
-       (8, 4, '22/11/2022 09:00', '22/11/2022 18:00', 'Absence Parents', 'Rien de particulier'),
-       (2, null, '15/11/2022 09:00', '15/11/2022 11:00', 'Rendez-vous', 'Rien de particulier'),
-       (3, 5, '19/11/2022 09:00', '20/11/2022 09:00', 'Absence Parents', 'Rien de particulier'),
-       (5, 3, '16/11/2022 09:00', '19/11/2022 09:00', 'Absence Parents', 'Rien de particulier'),
-       (10, null, '04/12/2022 09:00', '04/1/2022 11:00', 'Absence Parents', 'Rien de particulier'),
-       (9, null, '19/11/2022 09:00', '19/11/2022 15:00', 'Absence Parents', 'Rien de particulier'),
-       (6, 1, '14/11/2022 09:00', '14/11/2022 09:45', 'Rendez-vous', 'Rien de particulier'),
-       (6, null, '23/11/2022 09:00', '24/11/2022 09:00', 'Absence Parents', 'Hackathon');
+VALUES (1, null, '2022-11-12 09:00', '2022-11-13 09:00', 'Absence Parents', 'Rien de particulier'),
+       (8, 4, '2022-11-12 09:00', '2022-11-12 09:00', 'Absence Parents', 'Rien de particulier'),
+       (8, null, '2022-11-17 09:00', '2022-11-18 09:00', 'Absence Parents', 'Rien de particulier'),
+       (8, null, '2022-11-22 09:00', '2022-11-22 09:00', 'Absence Parents', 'Rien de particulier'),
+       (8, 4, '2022-11-22 09:00', '2022-11-22 18:00', 'Absence Parents', 'Rien de particulier'),
+       (2, null, '2022-11-15 09:00', '2022-11-15 11:00', 'Rendez-vous', 'Rien de particulier'),
+       (3, 5, '2022-11-19 09:00', '2022-11-20 09:00', 'Absence Parents', 'Rien de particulier'),
+       (5, 3, '2022-11-16 09:00', '2022-11-19 09:00', 'Absence Parents', 'Rien de particulier'),
+       (10, null, '2022-12-04 09:00', '2022-12-04 11:00', 'Absence Parents', 'Rien de particulier'),
+       (9, null, '2022-11-19 09:00', '2022-11-19 15:00', 'Absence Parents', 'Rien de particulier'),
+       (6, 1, '2022-11-14 09:00', '2022-11-14 09:45', 'Rendez-vous', 'Rien de particulier'),
+       (6, null, '2022-11-23 09:00', '2022-11-23 09:00', 'Absence Parents', 'Hackathon');
