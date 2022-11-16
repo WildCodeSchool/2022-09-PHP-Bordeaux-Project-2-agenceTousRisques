08c73bd802d136a2e71d4490adda718995fad0f2
zych.johann@gmail.com <zych.johann@gmail.com>
2022-11-16 11:04:22 +0100

    Fix Js Title

--- src/Model/AbstractManager.php
+++ src/Model/AbstractManager.php
@@ -39,8 +39,8 @@ abstract class AbstractManager
     public function selectOneById(int $id): array|false
     {
         // prepared request
-        $statement = $this->pdo->prepare("SELECT * FROM User INNER JOIN avatar
-    ON User.avatar=avatar.avatarID WHERE userID=:id");
+        $statement = $this->pdo->prepare("SELECT * FROM User INNER JOIN Avatar
+    ON User.avatar=Avatar.avatarID WHERE userID=:id");
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
 
