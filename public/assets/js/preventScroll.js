a61bf7e548bf211328052eba03a0a619b8a397b9
Jordan Lassalle <lassalle.jordan@gmail.com>
2022-11-17 20:33:12 +0100

    added the no scroll script everywhere it's needed, fix the gestion button on desktop header, and fixed the errors messages in modify profile

---
+++ public/assets/js/preventScroll.js
@@ -0,0 +1,14 @@
+function setScroll() {
+    let scroll = window.scrollY;
+    let scrollString = scroll.toString();
+    localStorage.setItem("scrollPosition", scrollString);
+}
+
+
+function restoreScrollPos() {
+    let posYString = localStorage.getItem("scrollPosition");
+    let posY = parseInt(posYString);
+    window.scroll(0, posY);
+    localStorage.setItem("scrollPosition", '0')
+    return true;
+}
